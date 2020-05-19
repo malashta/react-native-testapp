3ae786b8545e4cda7557603d8182711b6d9a0f39
Tallerr <malashta@gmail.com>
2020-05-19 09:40:02 +0700

    add project structure

--- App.js
+++ App.js
@@ -1,19 +1,54 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Navbar } from './src/Navbar';
+import { Addtodo } from './src/AddTodo';
+import { Todo } from './src/Todo';
 
 export default function App() {
+
+  const [todos, setTodos] = useState([]);
+
+  const AddTodos = (title) => {
+    console.log(title);
+    // const newTodo = {
+    //   id: Date.now().toString(),
+    //   title: title
+    // }
+
+    // setTodos(todos.concat([ newTodo ]))
+    // setTodos((prevTodos) => {
+    //   return [w
+    //     ...prevTodos,
+    //     newTodo
+    //   ]
+    // })
+
+    setTodos(prev => [
+      ...prev,
+      {
+        id: Date.now().toString(),
+        title
+      }
+    ])
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Navbar title="Todo app" />
+      <Addtodo onSubmit={AddTodos} />
+
+      <View>
+        {todos.map(todo => (
+          <Todo todo={todo} key={todo.id} />
+        ))}
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingHorizontal: 30,
+    paddingVertical: 20
   },
 });
